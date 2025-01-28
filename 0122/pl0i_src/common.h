627ec8dc561312db955412f758a084ef1126157e
tomochapee <tomoch123456@gmail.com>
2025-01-29 00:49:25 +0900

    データの肥やし

--- 0122/pl0i_src/common.h
+++
@@ -1,7 +0,0 @@
-/* ------------------------- common.h -------------------------- */
-typedef enum { FALSE, TRUE } bool;
-
-void error( char * );
-char *strsave( char * );
-void cleararray( char *, int );
-
